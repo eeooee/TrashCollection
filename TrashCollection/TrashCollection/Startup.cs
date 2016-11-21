8e226e1eecb4ce6e6b2296d830ad8508324fa7bf
eeooee <epiphany.compton@gmail.com>
2016-11-21 11:41:42 -0600

    Test commit

---
+++ TrashCollection/TrashCollection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollection.Startup))]
+namespace TrashCollection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
