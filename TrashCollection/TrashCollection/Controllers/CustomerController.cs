58d03f7f117d814136077c98a4450dd2f01ef5fb
eeooee <epiphany.compton@gmail.com>
2016-11-22 13:45:43 -0600

    starting to workout some layout ideas

---
+++ TrashCollection/TrashCollection/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrashCollection.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Portal()
+        {
+            return View();
+        }
+
+        public ActionResult Vacation()
+        {
+            return View();
+        }
+
+        public ActionResult Billing()
+        {
+            return View(); 
+        }
+
+        public ActionResult ChangePickUp()
+        {
+            return View();
+        }
+      
+    }
+}
---
+++ TrashCollection/TrashCollection/Controllers/EmployeePortal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrashCollection.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Portal()
+        {
+            return View();
+        }
+
+        public ActionResult GetRoute()
+        {
+            return View();
+        }
+
+        public ActionResult RouteList()
+        {
+            return View();
+        }
+
+        public ActionResult RouteMap()
+        {
+            return View();
+        }
+    }
+}
