9a1825e6dd4f224a3e949cd76885580222531766
ntirnanich <ntirnanich@yahoo.com>
2016-11-29 07:34:44 -0600

    PortalViewModel and Employee Portal

--- TrashCollection/TrashCollection/Controllers/EmployeePortal.cs
+++ TrashCollection/TrashCollection/Controllers/EmployeePortal.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrashCollection.Models;
+
 
 namespace TrashCollection.Controllers
 {
     public class EmployeeController : Controller
     {
+        ApplicationDbContext context;
+
+        public EmployeeController()
+        {
+            context = new ApplicationDbContext();
+        }
+
         public ActionResult Portal()
         {
-            return View();
+            var zipCodes = context.zipCodes.AsEnumerable().ToList();
+            return View(new PortalViewModel { ZipCodes = zipCodes });
         }
 
         public ActionResult GetRoute()
         {
-            return View();
+            Workers workerList = new Workers();
+          
+            return View();  //Return specific view "Workers"
         }
 
         public ActionResult RouteList()
         {
+           
             return View();
         }
 
