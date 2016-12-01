53bc66e4864f40d9361d88b863482a0c87c4b4f2
eeooee <epiphany.compton@gmail.com>
2016-12-01 09:21:15 -0600

    worker partial view added

---
+++ TrashCollection/TrashCollection/Controllers/WorkerProfileController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollection.Models;
+using TrashCollection.ViewModels;
+
+namespace TrashCollection.Controllers
+{
+    public class WorkerProfileController : Controller
+    {
+        ApplicationDbContext context;
+        public ActionResult WorkerProfile()
+        {
+            context = new ApplicationDbContext();
+            var model = new WorkerProfileViewModel();
+            return PartialView();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult WorkerProfile(WorkerProfileViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return PartialView(model);
+            }
+
+            var worker = new Workers { nickName = model.NickName, fullName = model.FullName };
+          
+            return PartialView(model);
+
+    }
+    }
+}
---
+++ TrashCollection/TrashCollection/ViewModels/WorkerProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrashCollection.Models;
+
+namespace TrashCollection.ViewModels
+{
+    public class WorkerProfileViewModel
+    {
+        public Workers workers { get; set; }
+        public string FullName { get; set; }
+        public string NickName { get; set; }
+
+    }
+}
