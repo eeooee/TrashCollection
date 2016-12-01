d294dba6ff90122d0857b989d15ba6f9c4165201
eeooee <epiphany.compton@gmail.com>
2016-12-01 10:11:06 -0600

    presentation

---
+++ TrashCollection/TrashCollection/Migrations/201612011528126_add-migration sample zips.cs
@@ -0,0 +1,16 @@
+namespace TrashCollection.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addmigrationsamplezips : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
